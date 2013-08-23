1e54b9700d5e67982969e9eea9ae760821a5ea77
Christian Sperandio <christian.sperandio@gmail.com>
2013-08-23 20:41:36 +0200

    Define the official first version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gemini "0.1.0-SNAPSHOT"
+(defproject gemini "0.1.0"
   :description "A data matching library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/chrix75/Gemini"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
