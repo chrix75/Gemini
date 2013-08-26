e9145c7b79086332b0d228b034376284bbc77592
Christian Sperandio <christian.sperandio@gmail.com>
2013-08-26 16:25:08 +0200

    Change version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gemini "0.3.0"
+(defproject gemini "0.3.1"
   :description "A data matching library"
   :url "https://github.com/chrix75/Gemini"
   :license {:name "Eclipse Public License"
